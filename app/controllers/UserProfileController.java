efbc197ab557ed3fb31d0b99a114a0d2b732d8dc
dfingerman <dmitriy.fingerman@esignlive.com>
2018-03-02 20:43:35 -0500

    Refactored twitter API authentication code into a separate common service.

--- app/controllers/UserProfileController.java
+++ app/controllers/UserProfileController.java
@@ -3,70 +3,42 @@ package controllers;
 import javax.inject.*;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import services.TwitterAuthenticator;
 
 import play.mvc.*;
 import play.libs.ws.*;
 import play.libs.ws.WSBodyReadables;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.Base64;
 import java.util.concurrent.CompletionStage;
 
 @Singleton
 public class UserProfileController extends Controller {
 	
-	private static final String TWITTER_CONSUMER_KEY = "2uiozTAH7aMj7zf3BfrXvajw0";
-	private static final String TWITTER_CONSUMER_SECRET = "8yeB9yu6bGG18CZu5fK23dQK6FgK2H2OJyA0uoY0Mv4LiTTnhP";
-	
-    private final WSClient ws;
+    private final WSClient wsClient;
+    private final TwitterAuthenticator twitterAuth;
     
     @Inject
-    public UserProfileController(WSClient ws) {
-      this.ws = ws;
+    public UserProfileController(WSClient wsClient, TwitterAuthenticator twitterAuth) {
+      this.wsClient = wsClient;
+      this.twitterAuth = twitterAuth;
     }
 
     public CompletionStage<Result> userProfile(String userName) {
     	
     	return
-    			getBearerCode(userName)
+    			twitterAuth.getAccessToken(userName)
                 .thenCompose(r -> getUserProfile(r, userName))
                 .thenApply(r -> ok(r));
-    }
-    
-    private CompletionStage<String> getBearerCode(String userName){
-    	return
-    			ws
-    			.url("https://api.twitter.com/oauth2/token")
-                .addHeader("User-Agent", "SOEN-6441")
-                .addHeader("Authorization", "Basic " + encodeKeys(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET))
-                .addHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
-                .addHeader("Content-Length", "29")
-                .post("grant_type=client_credentials")
-                .thenApply(r -> r.getBody(WSBodyReadables.instance.json()).get("access_token").asText());
     }
     
     private CompletionStage<JsonNode> getUserProfile(String accessToken, String userName){
 			
 		return 
-				ws
+				wsClient
 				.url("https://api.twitter.com/1.1/users/show.json")
 		        .addHeader("Authorization", "Bearer " + accessToken)
 		        .addQueryParameter("screen_name", userName)
 		        .get()
 		        .thenApply(r -> r.getBody(WSBodyReadables.instance.json()));
     }
-    
-	private static String encodeKeys(String consumerKey, String consumerSecret) {
-	    try {
-	        String encodedConsumerKey = URLEncoder.encode(consumerKey, "UTF-8");
-	        String encodedConsumerSecret = URLEncoder.encode(consumerSecret, "UTF-8");
-	        String fullKey = encodedConsumerKey + ":" + encodedConsumerSecret;
-	        byte[] encodedBytes = Base64.getEncoder().encode(fullKey.getBytes());
-	        return new String(encodedBytes);  
-	    }
-	    catch (UnsupportedEncodingException e) {
-	        return new String();
-	    }
-	}
 }
---
+++ app/services/TwitterAuthenticator.java
@@ -0,0 +1,51 @@
+package services;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Base64;
+import java.util.concurrent.CompletionStage;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import play.libs.ws.WSBodyReadables;
+import play.libs.ws.WSClient;
+
+@Singleton
+public class TwitterAuthenticator {
+	
+	private static final String TWITTER_CONSUMER_KEY = "2uiozTAH7aMj7zf3BfrXvajw0";
+	private static final String TWITTER_CONSUMER_SECRET = "8yeB9yu6bGG18CZu5fK23dQK6FgK2H2OJyA0uoY0Mv4LiTTnhP";
+	
+    private final WSClient wsClient;
+    
+    @Inject
+    public TwitterAuthenticator(WSClient wsClient) {
+      this.wsClient = wsClient;
+    }
+	
+    public CompletionStage<String> getAccessToken(String userName){
+    	return
+    			wsClient
+    			.url("https://api.twitter.com/oauth2/token")
+                .addHeader("User-Agent", "SOEN-6441")
+                .addHeader("Authorization", "Basic " + encodeKeys(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET))
+                .addHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+                .addHeader("Content-Length", "29")
+                .post("grant_type=client_credentials")
+                .thenApply(r -> r.getBody(WSBodyReadables.instance.json()).get("access_token").asText());
+    }
+    
+	private String encodeKeys(String consumerKey, String consumerSecret) {
+	    try {
+	        String encodedConsumerKey = URLEncoder.encode(consumerKey, "UTF-8");
+	        String encodedConsumerSecret = URLEncoder.encode(consumerSecret, "UTF-8");
+	        String fullKey = encodedConsumerKey + ":" + encodedConsumerSecret;
+	        byte[] encodedBytes = Base64.getEncoder().encode(fullKey.getBytes());
+	        return new String(encodedBytes);  
+	    }
+	    catch (UnsupportedEncodingException e) {
+	        return new String();
+	    }
+	}
+}
