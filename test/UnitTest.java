1af422e8404afb8109880c27dac3aed06090d0a5
Tumer Horloev <tumer.horloev@ericsson.com>
2018-03-05 23:00:21 -0500

    extra models testing 100% coverage for models and userProfile 88% for twitterAuthenticator

--- test/UnitTest.java
+++ test/UnitTest.java
@@ -178,6 +178,65 @@ public class UnitTest {
     		});
     	
     }
+    
+    @Test
+    public void testUserProfileAndTweetsModel() {
+    	UserProfile up = new UserProfile();
+    	up.setName("testName");
+    	up.setScreen_name("testSname");
+    	up.setCreated_at("createdAt");
+    	up.setDescription("testDesc");
+    	up.setFavourites_count(123123);
+    	up.setFollowers_count(123123);
+    	up.setFriends_count(123);
+    	up.setTime_zone("zone");
+    	up.setStatuses_count("sc");
+    	Tweet tweet1 = new Tweet();
+    	tweet1.setText("tweet1");
+    	tweet1.setCreated_at("tw1createdAt");
+    	Tweet tweet2 = new Tweet();
+    	tweet2.setText("tweet2");
+    	tweet2.setCreated_at("tw2createdAt");
+    	List<Tweet> tweets = new ArrayList<Tweet>();
+    	tweets.add(tweet1);
+    	tweets.add(tweet2);
+    	
+    	UserProfileAndTweets upat = new UserProfileAndTweets(new UserProfile(), new ArrayList<Tweet>());
+    	upat.setTweets(tweets);
+    	upat.setUserProfile(up);
+    	
+    	assertThat(upat.getUserProfile()==up);
+    	assertThat(upat.getTweets()==tweets);
+    	
+    	
+    }
+    
+    @Test
+    public void testUserProfileModel() {
+    	UserProfile up = new UserProfile();
+    	up.setName("testName");
+    	up.setScreen_name("testSname");
+    	up.setCreated_at("createdAt");
+    	up.setDescription("testDesc");
+    	up.setFavourites_count(123123);
+    	up.setFollowers_count(123123);
+    	up.setFriends_count(123);
+    	up.setTime_zone("zone");
+    	up.setStatuses_count("sc");
+    	Tweet tweet1 = new Tweet();
+    	tweet1.setText("tweet1");
+    	tweet1.setCreated_at("tw1createdAt");
+    	Tweet tweet2 = new Tweet();
+    	tweet2.setText("tweet2");
+    	tweet2.setCreated_at("tw2createdAt");
+    	List<Tweet> tweets = new ArrayList<Tweet>();
+    	tweets.add(tweet1);
+    	tweets.add(tweet2);
+    	
+    	up.setTweets(tweets);
+    	
+    	assertThat(up.getTweets()==tweets);
+    }
 
 }
 
