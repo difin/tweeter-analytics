b083ba131251032f3e0ca5570d18490f2bf528fe
dfingerman <dmitriy.fingerman@esignlive.com>
2018-03-12 22:41:37 -0400

    Added Javadoc for controller unit tests class

--- test/controllers/ApplicationControllerTest.java
+++ test/controllers/ApplicationControllerTest.java
@@ -35,24 +35,63 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.equalTo;
 
 /**
- * Test the functionality of Application Controller.
+ * Tests the functionality of Application Controller.
  * @author Deepika Dembla
  * @version 1.0.0
  *
  */
 public class ApplicationControllerTest {
 	
+	/**
+	 * Mock of User Profile service
+	 */
 	private UserProfileService userProfileService = mock(UserProfileService.class);
+	
+	/**
+	 * Mock of tweet search service
+	 */
 	private TenTweetsForKeywordService tenTweetsForKeywordService = mock(TenTweetsForKeywordService.class);
+	
+	/**
+	 * Mock of FormFactory
+	 */
 	private FormFactory formFactory = mock(FormFactory.class);
+	
+	/**
+	 * Mock of model that represents a user profile with 10 most recent tweets
+	 */
 	private UserProfileAndTweets userProfileAndTweets = mock(UserProfileAndTweets.class);
+	
+	/**
+	 * Mock of User Profile model
+	 */
 	private UserProfile userProfile = mock(UserProfile.class);
+	
+	/**
+	 * Mock of Strint form
+	 */
 	private Form<String> stringForm = mock(Form.class);
+	
+	/**
+	 * Mock of a field object
+	 */
 	private Field field = mock(Field.class);
 	
+	
+	/**
+	 * Execution context that encapsulates inside a Fork/Join pool.
+	 * This is a real object and not a mock because it is used to run async operations
+	 */
 	private HttpExecutionContext ec = new HttpExecutionContext(ForkJoinPool.commonPool());
+	
+	/**
+	 * A list of tweets
+	 */
 	private List<Tweet> tweets;
 	
+	/**
+	 * Initializes objects needed for tests before each unit test
+	 */
 	@Before
 	public void setup(){
 		
@@ -77,6 +116,14 @@ public class ApplicationControllerTest {
 		tweets.add(tweet2);
 	}
 
+	/**
+	 * Calls controller's index method and verifies that it returns a success status,
+	 * content of type text/html and that the output html page contains a certain text expected
+	 * on the home page.
+	 * 
+	 * @throws InterruptedException
+	 * @throws ExecutionException
+	 */
 	@Test
 	public void testIndex() throws InterruptedException, ExecutionException {
 		
@@ -90,6 +137,14 @@ public class ApplicationControllerTest {
 		assertThat(contentAsString(result).contains("Twitter Assignment 1"), is(equalTo(true)));
 	}
 	
+	/**
+	 * Calls controller's method that prepares a user profile page and expects
+	 * to get a success status, a content of type text/html and an html page that contains
+	 * a certain user's info.
+	 * 
+	 * @throws InterruptedException
+	 * @throws ExecutionException
+	 */
 	@Test
 	public void testUserProfile() throws InterruptedException, ExecutionException {
 		
@@ -108,6 +163,17 @@ public class ApplicationControllerTest {
 		assertThat(contentAsString(result).contains("some name"), is(equalTo(true)));
 	}
 	
+	
+	/**
+	 * Calls controller's search method with a non-empty search phrase.
+	 * Expects to get a response with a success status, a content of type text/html,
+	 * to have buttons in the output html (they allow opening a user profile page 
+	 * associated with the found tweet) and that the output html page will 
+	 * contain 2 expected tweets.
+	 * 
+	 * @throws InterruptedException
+	 * @throws ExecutionException
+	 */
 	@Test
 	public void testSearchWhenSearchStringIsNotEmpty() throws InterruptedException, ExecutionException {
 		
@@ -128,10 +194,19 @@ public class ApplicationControllerTest {
 		
 		assertThat(result.status(), is(equalTo(OK)));
 		assertThat(result.contentType().get(), is(equalTo("text/html")));
+		assertThat(contentAsString(result).contains("button"), is(equalTo(true)));
 		assertThat(contentAsString(result).contains("some tweet text 1"), is(equalTo(true)));
 		assertThat(contentAsString(result).contains("some tweet text 2"), is(equalTo(true)));
 	}
 	
+	/**
+	 * Calls controller's search method with an empty search phrase.
+	 * Expected to get a response with a success status, a content of type text/html
+	 * and not to have any buttons, since buttons are expected for found tweets.
+	 * 
+	 * @throws InterruptedException
+	 * @throws ExecutionException
+	 */
 	@Test
 	public void testSearchWhenSearchStringIsEmpty() throws InterruptedException, ExecutionException {
 		
@@ -147,5 +222,6 @@ public class ApplicationControllerTest {
 		
 		assertThat(result.status(), is(equalTo(OK)));
 		assertThat(result.contentType().get(), is(equalTo("text/html")));
+		assertThat(contentAsString(result).contains("button"), is(equalTo(false)));
 	}
 }
